df673f0f0b4f1f5bab0090b886c0adba22bb3de9
Srija S <srijakumar7@gmail.com>
2018-04-13 04:24:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
-
+def speak_to_grandma (phrase)
+  if phrase !== phrase.uppercase
+    return "HUH?! SPEAK UP, SONNY!"
 end
 
 # Whatever you say to grandma, she should respond with
