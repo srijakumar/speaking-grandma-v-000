8769b8a370c2269c08a6f3bf4bdb7beaf7a87af5
Srija S <srijakumar7@gmail.com>
2018-04-13 04:28:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,10 @@
 def speak_to_grandma (phrase)
   if phrase == phrase.uppercase()
     return "NO, NOT SINCE 1938!"
-  elsif return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+  return "HUH?! SPEAK UP, SONNY!"
 
   end
 end
